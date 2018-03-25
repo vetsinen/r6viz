bfa8de405ae4b76214f620a2f9fb23cc7447113e
vetsinen <vetsinen@gmail.com>
2018-03-25 15:53:01 +0300

    list added

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        lucky:42,
+        keys: [
+            {title:'working MacBook', permissions:'Read/Write', inidate:'Mar 29, 2018', centre: "Striputed Clubs"},
+            {title: 'my smartphone',permissions:'Read', inidate:'Apr 1, 2018', centre: "Mint Rhino"}
+        ]
+    }
+});
